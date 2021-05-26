7eab1f8bb36f7a7a1fb36e259d2ad455a8923d39
yuanjie111 <284522843@qq.com>
2021-05-26 22:17:36 +0800

    slice

--- src/ch7/map_test.go
+++ src/ch7/map_test.go
@@ -1,6 +1,8 @@
 package ch7
 
-import "testing"
+import (
+	"testing"
+)
 
 /**
 map的相关的测试
@@ -14,4 +16,18 @@ func TestInit(t *testing.T) {
 	m3["four"] = 4
 	t.Log(m3, len(m3))
 
+}
+
+func TestAccessNotExistingKey(t *testing.T) {
+	m := map[int]int{}
+	t.Log(m[1])
+	m[2] = 0
+	t.Log(m[2]) //都是0
+	//那么怎么判断值不存在了
+
+	if v, ok := m[3]; ok {
+		t.Log("exiting 3 %d", v)
+	} else {
+		t.Log("not existing 3")
+	}
 }
