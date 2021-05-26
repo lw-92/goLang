3f2781551159e0a2dd90544b2db03004b8f8063e
yuanjie111 <284522843@qq.com>
2021-05-26 20:27:37 +0800

    数组的比较

--- src/ch5/switch_test.go
+++ src/ch5/switch_test.go
@@ -18,3 +18,35 @@ func TestSwitch(t *testing.T) {
 		fmt.Print("default")
 	}
 }
+
+/**
+可以支持多个值
+*/
+func TestSwithMultiCase(t *testing.T) {
+
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("even")
+		case 1, 3:
+			t.Log("odd")
+
+		}
+	}
+}
+
+/**
+支持条件 做为switch 条件，这样和if就没有什么区别
+*/
+func TestSwithMultiCaseCondition(t *testing.T) {
+
+	for i := 0; i < 5; i++ {
+		switch {
+		case i%2 == 0:
+			t.Log("even")
+		case i%2 == 1:
+			t.Log("odd")
+
+		}
+	}
+}
