3cb8e37c6376f5c5a75848e551894a91fe649594
yuanjie111 <284522843@qq.com>
2021-05-28 08:07:03 +0800

    func

---
+++ src/ch11/object_test.go
@@ -0,0 +1,62 @@
+package ch11
+
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
+
+/**
+面向对象
+ 不支持继承
+new 返回的是指向实例的指针
+*/
+func TestCreateEmployee(t *testing.T) {
+	e := new(Employee)
+	e.Age = 10
+	e.Id = "1"
+	e.Name = "liwei"
+	employee := Employee{"0", "liwei1", 10}
+
+	e2 := Employee{Name: "liwei3", Age: 30}
+	t.Log(e, e2, employee) // 指针类型
+
+}
+
+/**
+对象的定义
+ 添加行为、方法
+*/
+type Employee struct {
+	Id   string
+	Name string
+	Age  int
+}
+
+/**
+这两种的区别
+这种会复制一个,会有跟多的内存开销
+*/
+func (e Employee) String() string {
+	fmt.Printf("adress is %x", unsafe.Pointer(&e.Name))
+	return fmt.Sprintf("id:%s-name :%s-age:%d", e.Id, e.Name, e.Age)
+}
+
+/**
+不会复制对象
+*/
+func (e *Employee) String1() string {
+	fmt.Printf("adress is %x", unsafe.Pointer(&e.Name))
+	return fmt.Sprintf("id:%s-name :%s-age:%d", e.Id, e.Name, e.Age)
+}
+
+func TestStructOperations(t *testing.T) {
+	employee := Employee{"0", "liwei", 20}
+	fmt.Printf("1111adress is %x", unsafe.Pointer(&employee.Name))
+	t.Log(employee.String())
+	fmt.Println("===============================")
+	// 使用指针访问,是可以调用的
+	e := &Employee{"0", "liwei", 20}
+	fmt.Printf("adress is %x", unsafe.Pointer(&e.Name))
+	t.Log(e.String1())
+}
