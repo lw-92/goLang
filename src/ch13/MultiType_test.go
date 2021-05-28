bf3b4f98e07bb8eaf30f25ff9cd32bf149213df1
yuanjie111 <284522843@qq.com>
2021-05-28 12:30:58 +0800

    空接口，object

--- src/ch13/MultiType_test.go
+++ src/ch13/MultiType_test.go
@@ -47,3 +47,35 @@ func TestPolymorphism(t *testing.T) {
 	writeFisrtProgram(goP)
 	writeFisrtProgram(javaP)
 }
+
+// 空接口和断言
+/**
+通过断言江空接口转换为制定类型
+*/
+
+func TestVoidInterface(t *testing.T) {
+	doSomeThing(2)
+	doSomeThing("33333")
+
+}
+
+func doSomeThing(p interface{}) {
+	//if i, ok := p.(int); ok {
+	//	fmt.Println("Integer", i)
+	//	return
+	//}
+	//if i, ok := p.(string); ok {
+	//	fmt.Println("string", i)
+	//	return
+	//}
+	//fmt.Println("unknown")
+	//另外一种写法
+	switch i := p.(type) {
+	case string:
+		fmt.Println("string", i)
+	case int:
+		fmt.Println("Integer", i)
+	default:
+		fmt.Println("unknown")
+	}
+}
